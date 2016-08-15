6ca89566f90fb872bd75be06faacc8ca8ceefc7d
James Newton-King <james@newtonking.com>
2016-08-15 15:36:50 +1200

    -Finish .NET Standard crypto fix

--- Src/Newtonsoft.Json.Schema/Infrastructure/Licensing/CryptographyHelpers.cs
+++ Src/Newtonsoft.Json.Schema/Infrastructure/Licensing/CryptographyHelpers.cs
@@ -22,19 +22,11 @@ namespace Newtonsoft.Json.Schema.Infrastructure.Licensing
         {
             bool valid;
 
-            //RSA rsa = RSA.Create();
-            //rsa.FromXmlString(PublicKey);
-            //rsa.
-
-#if true || (NETSTANDARD1_3)
+#if (NETSTANDARD1_3)
             RSA rsa = RSA.Create();
-            rsa.FromXmlString(PublicKey);
+            rsa.ImportParameters(ToRSAParameters(Convert.FromBase64String(PublicKeyCsp), false));
+
             valid = rsa.VerifyData(data, signature, HashAlgorithmName.SHA1, RSASignaturePadding.Pkcs1);
-
-            //RSACryptoServiceProvider rsaCryptoServiceProvider = new RSACryptoServiceProvider();
-            //rsaCryptoServiceProvider.ImportCspBlob(Convert.FromBase64String(PublicKeyCsp));
-
-            valid = rsaCryptoServiceProvider.VerifyData(data, SHA1.Create(), signature);
 #elif (PORTABLE && !NETSTANDARD1_3)
             try
             {
@@ -82,48 +74,91 @@ namespace Newtonsoft.Json.Schema.Infrastructure.Licensing
             return valid;
         }
 
+#if (NETSTANDARD1_3)
+        internal const int ALG_TYPE_RSA = (2 << 9);
+        internal const int ALG_CLASS_KEY_EXCHANGE = (5 << 13);
+        internal const int CALG_RSA_KEYX = (ALG_CLASS_KEY_EXCHANGE | ALG_TYPE_RSA | 0);
+
         internal static RSAParameters ToRSAParameters(this byte[] cspBlob, bool includePrivateParameters)
         {
-            try
+            BinaryReader br = new BinaryReader(new MemoryStream(cspBlob));
+
+            byte bType = br.ReadByte(); // BLOBHEADER.bType: Expected to be 0x6 (PUBLICKEYBLOB) or 0x7 (PRIVATEKEYBLOB), though there's no check for backward compat reasons. 
+            byte bVersion = br.ReadByte(); // BLOBHEADER.bVersion: Expected to be 0x2, though there's no check for backward compat reasons.
+            br.ReadUInt16(); // BLOBHEADER.wReserved
+            int algId = br.ReadInt32(); // BLOBHEADER.aiKeyAlg
+            if (algId != CALG_RSA_KEYX)
+                throw new PlatformNotSupportedException(); // The FCall this code was ported from supports other algid's but we're only porting what we use.
+
+            int magic = br.ReadInt32(); // RSAPubKey.magic: Expected to be 0x31415352 ('RSA1') or 0x32415352 ('RSA2') 
+            int bitLen = br.ReadInt32(); // RSAPubKey.bitLen
+
+            int modulusLength = bitLen/8;
+            int halfModulusLength = (modulusLength + 1)/2;
+
+            uint expAsDword = br.ReadUInt32();
+
+            RSAParameters rsaParameters = new RSAParameters();
+            rsaParameters.Exponent = ExponentAsBytes(expAsDword);
+            rsaParameters.Modulus = br.ReadReversed(modulusLength);
+            if (includePrivateParameters)
             {
-                BinaryReader br = new BinaryReader(new MemoryStream(cspBlob));
-
-                byte bType = br.ReadByte();    // BLOBHEADER.bType: Expected to be 0x6 (PUBLICKEYBLOB) or 0x7 (PRIVATEKEYBLOB), though there's no check for backward compat reasons. 
-                byte bVersion = br.ReadByte(); // BLOBHEADER.bVersion: Expected to be 0x2, though there's no check for backward compat reasons.
-                br.ReadUInt16();               // BLOBHEADER.wReserved
-                int algId = br.ReadInt32();    // BLOBHEADER.aiKeyAlg
-                if (algId != CALG_RSA_KEYX)
-                    throw new PlatformNotSupportedException();  // The FCall this code was ported from supports other algid's but we're only porting what we use.
-
-                int magic = br.ReadInt32();    // RSAPubKey.magic: Expected to be 0x31415352 ('RSA1') or 0x32415352 ('RSA2') 
-                int bitLen = br.ReadInt32();   // RSAPubKey.bitLen
-
-                int modulusLength = bitLen / 8;
-                int halfModulusLength = (modulusLength + 1) / 2;
-
-                uint expAsDword = br.ReadUInt32();
-
-                RSAParameters rsaParameters = new RSAParameters();
-                rsaParameters.Exponent = ExponentAsBytes(expAsDword);
-                rsaParameters.Modulus = br.ReadReversed(modulusLength);
-                if (includePrivateParameters)
-                {
-                    rsaParameters.P = br.ReadReversed(halfModulusLength);
-                    rsaParameters.Q = br.ReadReversed(halfModulusLength);
-                    rsaParameters.DP = br.ReadReversed(halfModulusLength);
-                    rsaParameters.DQ = br.ReadReversed(halfModulusLength);
-                    rsaParameters.InverseQ = br.ReadReversed(halfModulusLength);
-                    rsaParameters.D = br.ReadReversed(modulusLength);
-                }
-
-                return rsaParameters;
+                rsaParameters.P = br.ReadReversed(halfModulusLength);
+                rsaParameters.Q = br.ReadReversed(halfModulusLength);
+                rsaParameters.DP = br.ReadReversed(halfModulusLength);
+                rsaParameters.DQ = br.ReadReversed(halfModulusLength);
+                rsaParameters.InverseQ = br.ReadReversed(halfModulusLength);
+                rsaParameters.D = br.ReadReversed(modulusLength);
             }
-            catch (EndOfStreamException)
-            {
-                // For compat reasons, we throw an E_FAIL CrytoException if CAPI returns a smaller blob than expected.
-                // For compat reasons, we ignore the extra bits if the CAPI returns a larger blob than expected.
-                throw E_FAIL.ToCryptographicException();
-            }
+
+            return rsaParameters;
         }
+
+        /// <summary>
+        /// Helper for converting a UInt32 exponent to bytes.
+        /// </summary>
+        private static byte[] ExponentAsBytes(uint exponent)
+        {
+            if (exponent <= 0xFF)
+            {
+                return new[] {(byte) exponent};
+            }
+            if (exponent <= 0xFFFF)
+            {
+                return new[]
+                {
+                    (byte) (exponent >> 8),
+                    (byte) (exponent)
+                };
+            }
+            if (exponent <= 0xFFFFFF)
+            {
+                return new[]
+                {
+                    (byte) (exponent >> 16),
+                    (byte) (exponent >> 8),
+                    (byte) (exponent)
+                };
+            }
+
+            return new[]
+            {
+                (byte) (exponent >> 24),
+                (byte) (exponent >> 16),
+                (byte) (exponent >> 8),
+                (byte) (exponent)
+            };
+        }
+
+        /// <summary>
+        /// Read in a byte array in reverse order.
+        /// </summary>
+        private static byte[] ReadReversed(this BinaryReader br, int count)
+        {
+            byte[] data = br.ReadBytes(count);
+            Array.Reverse(data);
+            return data;
+        }
+#endif
     }
 }
